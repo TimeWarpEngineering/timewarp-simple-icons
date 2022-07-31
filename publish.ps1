e62ed5d8a4f837de9d2cf0da4d9d722bfbabbda5
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-07-31 07:28:43 +0000

    add tool to manifest and update description

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,8 @@
 Push-Location $PSScriptRoot
 try {
     if (!$Nuget_Key) { throw "Nuget_Key is not set"}
+    dotnet tool restore
+    dotnet cleanup -y
     dotnet pack ./Source/timewarp-simple-icons/timewarp-simple-icons.csproj -c Release
     dotnet nuget push **/*.nupkg --source https://api.nuget.org/v3/index.json --api-key $Nuget_Key    
 }
