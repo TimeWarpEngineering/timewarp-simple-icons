832a789c10a1611d7ed2ed55d773f62552d99a5e
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2022-10-29 22:53:08 +0000

    transform all again to get LF instead of CRLF

--- transform.ps1
+++ transform.ps1
@@ -1,7 +1,7 @@
 ﻿Push-Location $PSScriptRoot/tools/transform
 try {
   if (!$simple_icons) { throw "simple_icons should be set to the root path of where you cloned the simple-icons repo"}
-  dotnet run --project transform.csproj -- "$simple_icons\icons" "$PSScriptRoot\..\Source\timewarp-simple-icons\icons"
+  dotnet run --project transform.csproj -- "$simple_icons\icons" "$PSScriptRoot\Source\timewarp-simple-icons\icons"
 }
 finally {
     Pop-Location
